4a8def1996e99ef8f44fe75e3ed621e8b1d4132d
Dee <96267926+deedoodee@users.noreply.github.com>
2022-02-20 02:32:38 -0500

    Update db.js

--- server/db.js
+++ server/db.js
@@ -2,7 +2,7 @@ const Pool = require('pg').Pool;
 
 const pool = new Pool({
     user: "postgres",
-    password: "elephant31NYCS",
+    password: "",
     host: "localhost",
     port: 5432,
     database: "lavendrfitness"
